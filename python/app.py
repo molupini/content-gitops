0d40311dfefd4a3f4c66cbd3f3dcba2c6dc8ffdd
System Administrator <maurizio.lupini@rapiddeploy.com>
2020-09-21 14:17:19 +0200

    updated app.py v2

--- python/app.py
+++ python/app.py
@@ -3,11 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-<<<<<<< HEAD
-    return "Hello World! From Maurizio Lupini, Updated : ) v1"
-=======
-    return "Hello World! From Maurizio Lupini, Updated : )"
->>>>>>> 71f5e18246b3263d1039efc993c093f602cf0843
+    return "Hello World! From Maurizio Lupini, Updated : ) v2"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
